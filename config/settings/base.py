05d4b2d57f4d9a43c6533a894d1fd89eadd9e652
Lee HyunJoo <wayhome250@gmail.com>
2017-11-25 18:29:52 +0900

    install django-rest-auth, add login, logout, signup add migration file

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,9 @@ THIRD_PARTY_APPS = [
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
     'rest_framework',  # REST framework
+    'rest_framework.authtoken',
+    'rest_auth',  # rest auth
+    'rest_auth.registration',  # enable registration
     'taggit',  # Tags for the photos
     'taggit_serializer',  # Tag serializer
 ]
@@ -264,8 +267,6 @@ SOCIALACCOUNT_ADAPTER = 'nomadgram.users.adapters.SocialAccountAdapter'
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'users:redirect'
-LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
@@ -285,8 +286,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
+REST_USE_JWT = True
+
+ACCOUNT_LOGOUT_ON_GET = True
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^users/', include('nomadgram.users.urls', namespace='users')),
     url(r'images/', include('nomadgram.images.urls', namespace='images')),
     url(r'notifications/', include('nomadgram.notifications.urls', namespace='notifications'))
